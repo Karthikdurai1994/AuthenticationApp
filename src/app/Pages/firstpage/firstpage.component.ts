eb4f77deb633d91643cbbf6195da625626bb816d
Karthikdurai1994 <duraikarthik6@gmail.com>
2020-10-17 21:15:19 +0530

    Changed chatbot link

--- src/app/Pages/firstpage/firstpage.component.ts
+++ src/app/Pages/firstpage/firstpage.component.ts
@@ -24,18 +24,18 @@ export class FirstpageComponent implements OnInit {
 
     console.log("Came to chatbot function");
     var BotStar={appId:"s4f323620-c0ca-4358-9da6-605dbfdcacb5",mode:"livechat"};
-    !function(t,a){
-       var e = ()=>{
+    !function(t,a): any{
+       var e = function(){
         (e["q"]=e["q"]||[]).push(arguments)
        };
-       e["q"]=e["q"]||[],t.BotStarApi=e;
-       !function(){
+       e["q"]=e["q"]||[],t["BotStarApi"]=e;
+       !function(): any{
         var t=a.createElement("script");
-        t.type="text/javascript",t.async=1,t.src="https://widget.botstar.com/static/js/widget.js";
+        t.type="text/javascript",t.async=true,t.src="https://widget.botstar.com/static/js/widget.js";
         var e=a.getElementsByTagName("script")[0];
         e.parentNode.insertBefore(t,e)
-       };
-    }
+       }();
+    }(window,document)
 
   }
 
