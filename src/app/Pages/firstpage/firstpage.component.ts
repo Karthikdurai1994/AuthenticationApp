d238d9bcdd46feb10e4e3d18df6013262bb004e4
Karthikdurai1994 <duraikarthik6@gmail.com>
2020-10-20 16:19:45 +0530

    Modified Chatbot link 41

--- src/app/Pages/firstpage/firstpage.component.ts
+++ src/app/Pages/firstpage/firstpage.component.ts
@@ -43,34 +43,37 @@ export class FirstpageComponent implements OnInit, AfterViewInit  {
      userTokens: "The Witcher",
     }
   };
-  window.BotStarApi('boot', BotStar);
-  // !function(t,a):any{
-  //    console.log("Function 1");
+  //window.BotStarApi('boot', BotStar);
+  !function(t,a):any{
+     console.log("Function 1");
 
-  //      var e = function(){(e["q"]=e["q"]||[]).push(arguments)};
-  //      e["q"]=e["q"]||[];
-  //      console.log("e is: ", e);
-  //      t["BotStarApi"]=e;
-  //      !function():any{
-  //       const body = <HTMLDivElement> a.body;
-  //       var t=a.createElement('script');
-  //       t.innerHTML = '';
-  //       t.type="text/javascript",
-  //       t.src="https://widget.botstar.com/static/js/widget.js"
-  //       t.async = false;
-  //       t.defer = true;
-  //       //var e=a.getElementsByTagName("script")[0];
-  //       body.appendChild(t);
-  //     //  t.parentNode.insertBefore(e, t.nextSibling);
-  //       //e.parentNode.insertBefore(t,e)
-  //      // e.parentNode.insertBefore(t,e)
+       var e = function(){(e["q"]=e["q"]||[]).push(arguments)};
+       e["q"]=e["q"]||[];
+       console.log("e is: ", e);
+       t["BotStarApi"]=e;
+       !function():any{
+        const body = <HTMLDivElement> a.body;
+        var t=a.createElement('script');
+        t.innerHTML = '';
+        t.type="text/javascript",
+        t.src="https://widget.botstar.com/static/js/widget.js"
+        t.async = false;
+        t.defer = true;
+        //var e=a.getElementsByTagName("script")[0];
+        setTimeout(()=>{
+          body.appendChild(t);
+        }, 20000);
 
-  //       console.log("t is: ",t);
-  //       console.log("e is: ", e);
-  //       //body.appendChild(t);
-  //      }();
+      //  t.parentNode.insertBefore(e, t.nextSibling);
+        //e.parentNode.insertBefore(t,e)
+       // e.parentNode.insertBefore(t,e)
 
-  //   }(window,document)
+        console.log("t is: ",t);
+        console.log("e is: ", e);
+        //body.appendChild(t);
+       }();
+
+    }(window,document)
 
   }
 
